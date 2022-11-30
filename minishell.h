9f7686956577531efc06db75045f70eda2fa97dc
Milan Stojiljkovic <drownedmilan@gmail.com>
2022-11-30 19:13:12 +0100

    merging .h

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jibot <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: mstojilj <mstojilj@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 10:58:48 by jibot             #+#    #+#             */
-/*   Updated: 2022/03/23 17:49:12 by jibot            ###   ########.fr       */
+/*   Updated: 2022/11/30 19:07:59 by mstojilj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,40 @@ typedef struct s_cmd
 	struct s_cmd	*next;
 }	t_cmd;
 
-int	ft_abs(int a);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_strlen(char *str);
-int	ft_tablen(char **tab);
-char	*ft_strdup(char *str);
-char	*ft_strjoin(char *s1, char *s2);
-char	**token_join(char **args, char *token);
-int	is_delim(char c);
-char	*get_env_vars(char *token, char **env_vars);
-char	**parsing(char **lex_tab, char **env_vars);
-char	**lexing(char *line);
+typedef struct s_env
+{
+	char			*line;
+	struct s_env	*next;
+} t_env;
+
+/***********BASIC**********/
+int		ft_isspace(char c);
+void	ft_strcpy(char *dst, char *src);
+
+/***********LIBFT**********/
+int		ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strlen(char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+/***********ENV************/
+void	ft_get_env(t_env **env_list, char **env);
+void	ft_print_env(t_env **env_lst);
+void	ft_add_queue(t_env **root, char *s);
+
+/***********ECHO***********/
+void	ft_echo(char *s, int option);        // Echo function
+void	ft_do_echo(char *s);				 // Echo (no options)
+void	ft_do_nl_echo(char *s);				 // Echo -n
+int		ft_get_echo_line(char *s);			 // Gets the echo line
+int		ft_get_echo_n(char *s, int start);	 // Gets the echo -n line
+
+/***********EXPORT*********/
+void	ft_export(t_env **env_list, char *line);
+void	ft_add_after(t_env **env_list, int line_nb, char *s);
+
+
+/***********UNSET**********/
+void	ft_unset(t_env **env_list, char *s);
+void	ft_remove_line(t_env **env_list, t_env *node);
 
 #endif
