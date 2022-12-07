b2d52880bd323aa975ba5decf7c4a9334cfe0b4e
Jules Ibot <julzibot@MacBook-Air-de-Jules.local>
2022-12-08 00:28:08 +0100

    doing concats

--- create_env_vars.c
+++ create_env_vars.c
@@ -1,25 +1,63 @@
 #include "minishell.h"
 
+static  char *check_concat(char *cpy)
+{
+    int i;
+    char    *str;
+
+    i = -1;
+    while (cpy && cpy[++i] && cpy[i] != '=')
+        ;
+    if (cpy && cpy[i] && cpy[i] == '=')
+    {
+        if (cpy[i - 1] && cpy[i - 1] == '+')
+        {
+            i = -1;
+            str = malloc(ft_strlen(cpy));
+            while (cpy[++i] != '+')
+                str[i] = cpy[i];
+            while (cpy[++i])
+                str[i - 1] = cpy[i];
+            str[i - 1] = '\0';
+            free(cpy);
+            return (str);
+        }
+        else
+            return (cpy);
+
+    }
+    return(NULL);
+}
+
 static  char	**create_var(char **env_vars, char *cpy)
 {
 	int	j;
 	int	namelen;
+    int concat;
 
 	j = 0;
 	while (env_vars && env_vars[j])
 	{
+        concat = 0;
 		namelen = 0;
 		while (env_vars[j][namelen] != '=')
 			namelen++;
-		if (!ft_strncmp(env_vars[j], cpy, namelen) && cpy[namelen] == '=')
+        if (cpy && cpy[namelen] && cpy[namelen] == '+' && cpy[namelen + 1] == '=')
+            concat = 1;
+		if (!ft_strncmp(env_vars[j], cpy, namelen) && cpy[namelen + concat] == '=')
 		{
-			free(env_vars[j]);
-			env_vars[j] = ft_strdup(cpy);
+            if (!concat)
+            {
+                free(env_vars[j]);
+			    env_vars[j] = ft_strdup(cpy);
+            }
+            else   
+                env_vars[j] = ft_strjoin(env_vars[j], ft_strdup(cpy + namelen + concat + 1));
 			return (env_vars);
 		}
 		j++;
 	}
-	env_vars = token_join(env_vars, ft_strdup(cpy));
+	env_vars = token_join(env_vars, check_concat(cpy));
 	return (env_vars);
 }
 
@@ -32,7 +70,7 @@ char	**create_env_vars(char	*token, char **env_vars) //search for NAME=VALUE in 
 	cpy = get_env_vars(token, env_vars);
 	while (cpy && cpy[i] && cpy[i] != '=')
 		i++;
-	if (cpy && cpy[i] && cpy[i] == '=') 
+	if (cpy && cpy[i] && cpy[i] == '=')
 		env_vars = create_var(env_vars, cpy);
 	free(token);
 	free(cpy);
