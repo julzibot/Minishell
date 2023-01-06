5549bfae36e2c6ce3e86e3c16d100720ff133d6d
Milan Stojiljkovic <drownedmilan@gmail.com>
2023-01-06 17:36:25 +0100

    06/01 main Milan

--- Minishell.c
+++ Minishell.c
@@ -6,19 +6,22 @@
 /*   By: mstojilj <mstojilj@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 10:58:48 by jibot             #+#    #+#             */
-/*   Updated: 2023/01/04 18:35:22 by mstojilj         ###   ########.fr       */
+/*   Updated: 2023/01/06 17:31:01 by mstojilj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	parse_init(t_cmd *parse_list)
+void	parse_init(t_cmd *parse_list, char **envp)
 {
-	if (!parse_list)
-		exit(1);
 	parse_list->env_vars = NULL;
 	parse_list->quoted = NULL;
 	parse_list->space_after = NULL;
+	parse_list->env_list = NULL;
+	parse_list->exp_list = NULL;
+	ft_get_env(&parse_list->env_list, envp); // For env command
+	ft_get_env(&parse_list->exp_list, envp); // For export command
+	ft_get_export(&parse_list->exp_list);    // Declare -x PWD="somewhere/nice/and/cozy"
 }
 
 void	check_line_exists(char *line)
@@ -40,18 +43,10 @@ int	main(int argc, char **argv, char **envp)
 	char				**tokens;
 	t_cmd 				*parse_list;
 
-	/*Milan 16/12*/
-	t_env			 	*env_list;
-	t_env				*exp_list;
-	
-	env_list = NULL;
-	exp_list = NULL;
-	ft_get_env(&env_list, envp); // For env command
-	ft_get_env(&exp_list, envp); // For export command
-	ft_get_export(&exp_list);    // Declare -x PWD="somewhere/nice/and/cozy"
-	/*Milan 16/12*/
 	parse_list = malloc(sizeof(t_cmd));
-	parse_init(parse_list);
+	if (!parse_list)
+		exit(1);
+	parse_init(parse_list, envp);
 	while (1)
 	{
 		signal(SIGQUIT, SIG_IGN);
@@ -61,7 +56,9 @@ int	main(int argc, char **argv, char **envp)
 		add_history(line);
 		tokens = lexing(line, parse_list);
 		parse_list = parsing(tokens, parse_list);
-		ft_exec_cmd(parse_list, &env_list, &exp_list, envp);
+		ft_exec_cmd(parse_list, envp);
+		printf("enters\n");
+		ft_print_env(parse_list->env_list);
 	}
 	return (0);
 }
